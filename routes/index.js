6800d245c980a0f0500421afd8aa85a55fd8afcc
mlekena <mlekena@skidmore.edu>
2016-03-26 01:13:32 -0400

    fixed routing bugs

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,6 @@
 //ROUTER
 var express = require('express');
 var router = express.Router();
-var appUsers = require('../testusers.json');
 
 router.get('/', function(request,response){
 	response.render('index');
